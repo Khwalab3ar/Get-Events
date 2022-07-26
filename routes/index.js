008a7610eb4b1a77e8e45afd6c7e775d7e02e44c
Khwa Oo <khwalabear@gmail.com>
2022-07-25 20:35:16 -0400

    added server.js file

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const { Router } = require('express')
+const router = Router()
+const orgController = require('../controllers/OrganizationController')
+const eventController = require('../controllers/EventController')
+const personalController = require('../controllers/PersonalController')
+
+router.get('/', (req, res) => {
+  res.send('this is the root!')
+})
+router.get('/organizations', orgController.getAllOrgs)
+router.get('/events', eventController.getAllEvents)
+router.get('/personals', personalController.getAllPersonals)
+module.exports = router
