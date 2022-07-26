e47e72d1013cfcf269b75c65d26b45c372b97b89
Khwa Oo <khwalabear@gmail.com>
2022-07-26 10:05:56 -0400

    Event controllers added

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -1,9 +1,56 @@
+const { response } = require('express')
 const Event = require('../models/events')
 
 const getAllEvents = async (req, res) => {
   try {
     const allEvents = await Event.find()
     return res.status(200).json(allEvents)
+  } catch (e) {
+    return res.status(500).send(e.message)
+  }
+}
+
+const searchEvents = async (req, res) => {
+  try {
+    const events = await Event.find(req.body)
+    if (events) {
+      return res.status(200).json({ events })
+    }
+    return res.status(404).send('No events were found.')
+  } catch (e) {
+    return res.status(500).send(e.message)
+  }
+}
+
+const updateEvent = async (req, res) => {
+  try {
+    const { id } = req.params
+    const event = await Event.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true },
+      (err, event) => {
+        if (err) {
+          res.status(500).send(err)
+        }
+        if (!event) {
+          res.status(500).send('Event was not found')
+        }
+      }
+    )
+  } catch (e) {
+    return res.status(500).send(e.message)
+  }
+}
+
+const deleteEvent = async (req, res) => {
+  try {
+    const { id } = req.params
+    const event = await Event.findByIdAndDelete(id)
+    if (event) {
+      return res.status(200).send('Event was deleted')
+    }
+    throw new Error('event was not found')
   } catch (e) {
     return res.status(500).send(e.message)
   }
--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,14 @@ router.get('/', (req, res) => {
 router.get('/organizations', orgController.getAllOrgs)
 router.get('/events', eventController.getAllEvents)
 router.get('/personals', personalController.getAllPersonals)
+router.get('/organization/search', orgController.searchOrganizations)
+router.get('/event/search', eventController.searchEvents)
+router.get('/personal/search', personalController.searchPersonals)
+router.put('/organization/:id', orgController.updateOrg)
+router.put('/event/:id', eventController.updateEvent)
+router.put('/personal/:id', personalController.updatePersonal)
+router.delete('/organization/:id', orgController.deleteOrg)
+router.delete('/event/:id', eventController.deleteEvent)
+router.delete('/personal/:id', personalController.deletePersonal)
 
 module.exports = router
