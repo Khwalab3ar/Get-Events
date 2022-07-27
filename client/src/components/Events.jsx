bb1ffb4f38c087856933efe7a4ea39ff37b6f802
Khwa Oo <khwalabear@gmail.com>
2022-07-26 20:16:54 -0400

    Home events rendered

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -1,11 +1,28 @@
 import { useState,useEffect } from "react"
+import axios from 'axios'
+import { BASE_URL } from "../globals"
+
+
 const Event = () =>{
-  const getAllEvent = () =>{
-
-  }
-
+  const[events,setEvents]= useState([])
+  useEffect(()=>{
+    const getAllEvent = async () =>{
+      const res = await axios.get(`${BASE_URL}events`) 
+      setEvents(res.data)  
+    }
+    getAllEvent()
+    console.log(events)
+  },[])
   return(
-    <div></div>
+    <div className="events-grid">
+      {events.map((event)=>(
+        <div key={event._id}>
+          <h1>{event.name}</h1>
+          <h3>Industry: {event.industy}</h3>
+          <h3>Price: <span className="price">${event.price}</span></h3>
+      </div>
+      ))}
+    </div>
   )
 }
 
